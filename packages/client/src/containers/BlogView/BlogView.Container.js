8ae23f6aa975e47a3475c546cfff2d9c61fa78f6
Andrii Gorokhovskyi <andrii.gorokhovskyi@gmail.com>
2025-04-15 15:45:54 +0200

    wip

--- packages/client/src/containers/BlogView/BlogView.Container.js
+++ packages/client/src/containers/BlogView/BlogView.Container.js
@@ -14,8 +14,10 @@ import Modal from '../../components/Modal/Modal.Component';
 import iconCopy from '../../assets/images/icons8-copy-24.png';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import Markdown from 'markdown-to-jsx';
+import logo from '../../assets/images/logo.png';
 
 import { getDateFromTimestamp } from '../../utils/getDateFromTimestamp';
+import { getEstimatedReadTime } from '../../utils/getEstimatedReadTime';
 
 import {
   faEnvelope,
@@ -28,12 +30,16 @@ import {
   faFacebookF,
   faTwitter,
   faLinkedinIn,
+  faPinterest,
 } from '@fortawesome/free-brands-svg-icons';
 import {
+  PinterestShareButton,
   FacebookShareButton,
   TwitterShareButton,
   LinkedinShareButton,
   EmailShareButton,
+  FacebookShareCount,
+  PinterestShareCount,
 } from 'react-share';
 import appImage from '../../assets/images/app-placeholder.svg';
 import { faHeart } from '@fortawesome/free-regular-svg-icons';
@@ -57,6 +63,8 @@ export const BlogView = () => {
     fetchSingleBlog(slugParam);
   }, [slugParam]);
 
+  const readTime = getEstimatedReadTime(blog?.content);
+
   console.log(slugParam);
   console.log(blog);
 
@@ -72,6 +80,7 @@ export const BlogView = () => {
         </header>
         <main>
           <article>
+            <p>{readTime} min read</p>
             <Markdown>{blog.content}</Markdown>
             <footer>
               <p>
@@ -81,6 +90,66 @@ export const BlogView = () => {
                 </time>{' '}
                 by <strong>{blog?.userFullName?.split(' ')[0]}</strong>
               </p>
+              <div className="icons-apps-page">
+                <span>Share it: </span>
+                <FontAwesomeIcon
+                  icon={faLink}
+                  className="button-copy"
+                  onClick={() => {
+                    navigator.clipboard.writeText(
+                      `https://www.motivately.co/blog/${blog.slug}`,
+                    );
+                  }}
+                />
+                <PinterestShareButton
+                  media={logo}
+                  description={blog.meta_description || ''}
+                >
+                  <FontAwesomeIcon className="share-icon" icon={faPinterest} />
+                </PinterestShareButton>
+                <FacebookShareButton
+                  url={`https://www.motivately.co/blog/${blog.slug}`}
+                >
+                  <FontAwesomeIcon className="share-icon" icon={faFacebookF} />
+                </FacebookShareButton>
+                <TwitterShareButton
+                  url={`https://www.motivately.co/blog/${blog.slug}`}
+                  title={`'${blog.title}'`}
+                  hashtags={['quotes', 'inspirational']}
+                >
+                  <FontAwesomeIcon className="share-icon" icon={faTwitter} />
+                </TwitterShareButton>
+                <LinkedinShareButton
+                  url={`https://www.motivately.co/blog/${blog.slug}`}
+                >
+                  <FontAwesomeIcon className="share-icon" icon={faLinkedinIn} />
+                </LinkedinShareButton>
+                <EmailShareButton
+                  subject="Check out this blog!"
+                  body={`It is so inspirational: '${blog.title}'`}
+                  url={`https://www.motivately.co/blog/${blog.slug}`}
+                >
+                  <FontAwesomeIcon icon={faEnvelope} />
+                </EmailShareButton>
+              </div>
+              <div>
+                <FacebookShareCount
+                  url={`https://www.motivately.co/blog/${blog.slug}`}
+                >
+                  {(shareCount) => (
+                    <span className="myShareCountWrapper">{shareCount}</span>
+                  )}
+                </FacebookShareCount>
+                <PinterestShareCount
+                  url={`https://www.motivately.co/blog/${blog.slug}`}
+                >
+                  {(shareCount) =>
+                    shareCount > 0 && (
+                      <span className="myShareCountWrapper">{shareCount}</span>
+                    )
+                  }
+                </PinterestShareCount>
+              </div>
             </footer>
           </article>
         </main>
