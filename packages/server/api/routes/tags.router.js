4290c3892f664606ae544b2124646282c69f71d0
Andrii Gorokhovskyi <andrii.gorokhovskyi@gmail.com>
2025-04-21 17:12:38 +0200

    wip

--- packages/server/api/routes/tags.router.js
+++ packages/server/api/routes/tags.router.js
@@ -29,4 +29,17 @@ router.get('/', (req, res, next) => {
   }
 });
 
+router.post('/', (req, res) => {
+  const { token } = req.headers;
+  tagsController
+    .createTag(token, req.body)
+    .then((result) => res.json(result))
+    .catch((error) => {
+      // eslint-disable-next-line no-console
+      console.log(error);
+
+      res.status(400).send('Bad request').end();
+    });
+});
+
 module.exports = router;
