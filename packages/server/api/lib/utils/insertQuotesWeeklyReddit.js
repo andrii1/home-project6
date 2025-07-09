b5c4730603242f05ca19ce82fd1541df9fda2e2a
Andrii Gorokhovskyi <andrii.gorokhovskyi@gmail.com>
2025-07-09 17:09:30 +0200

    wip

--- packages/server/api/lib/utils/insertQuotesWeeklyReddit.js
+++ packages/server/api/lib/utils/insertQuotesWeeklyReddit.js
@@ -2,6 +2,14 @@
 const scrapeQuotesReddit = require('./scrapeQuotesReddit');
 const insertQuotes = require('./insertQuotes');
 
+const today = new Date();
+const isSunday = today.getDay() === 0; // 0 = Sunday
+
+if (!isSunday) {
+  console.log('Not Sunday, skipping weekly job.');
+  process.exit(0);
+}
+
 const insertQuotesReddit = async () => {
   try {
     const quotes = await scrapeQuotesReddit();
